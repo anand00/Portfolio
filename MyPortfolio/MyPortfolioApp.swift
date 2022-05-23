424ad4567f3e7c0a56a971c4b2e01d92ab18100f
Anand Praksh <anand.p@osmosys.asia>
2022-05-23 09:39:50 +0530

    Project Setup

---
+++ MyPortfolio/MyPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPortfolioApp.swift
+//  MyPortfolio
+//
+//  Created by Anand Praksh on 23/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MyPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
